5a69d7fa8b7e5210b7b2de4a98481dfc4205be2f
LLLarry <1079485906@qq,com>
2020-05-04 14:32:37 +0800

    商户缴费添加07，08设备

--- src/views/SystemSet/SystemTem/components/PayTem/index.vue
+++ src/views/SystemSet/SystemTem/components/PayTem/index.vue
@@ -8,66 +8,27 @@
              <el-row class="payTemRow">
                 <el-col :span="12" class="netWork">
                     <div class="title">网络设备模板</div>
-                     <el-row class="item">
-                         <el-col :span="10">00出厂默认设置</el-col>
-                         <el-col :span="14"><el-input-number v-model="payTemData['00']['00']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
-                     </el-row>
-                     <el-row class="item">
-                         <el-col :span="10">01十路智慧款</el-col>
-                        <el-col :span="14"><el-input-number v-model="payTemData['00']['01']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
-                     </el-row>
-                     <el-row class="item">
-                         <el-col :span="10">02电轿款</el-col>
-                        <el-col :span="14"><el-input-number v-model="payTemData['00']['02']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
-                     </el-row>
-                     <el-row class="item">
-                         <el-col :span="10">03脉冲板子</el-col>
-                        <el-col :span="14"><el-input-number v-model="payTemData['00']['03']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
-                     </el-row>
-                     <el-row class="item">
-                         <el-col :span="10">04离线充值机</el-col>
-                        <el-col :span="14"><el-input-number v-model="payTemData['00']['04']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
-                     </el-row>
-
-                     <el-row class="item">
-                         <el-col :span="10">05十六路智慧款</el-col>
-                        <el-col :span="14"><el-input-number v-model="payTemData['00']['05']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
-                     </el-row>
-                      <el-row class="item">
-                         <el-col :span="10">06二十路智慧款</el-col>
-                        <el-col :span="14"><el-input-number v-model="payTemData['00']['06']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
+                    <el-row class="item" v-for="(val,key) in payTemData['00']" :key="key">
+                         <el-col :span="10">{{key}}{{ 
+                             key === '00' ? '出厂默认设置' : 
+                             key === '01' ? '十路智慧款' : 
+                             key === '02' ? '电轿款': 
+                             key === '03' ? '脉冲板子' : 
+                             key === '04' ? '离线充值机' : 
+                             key === '05' ? '十六路智慧款' : 
+                             key === '06' ? '二十路智慧款' : 
+                             key === '07' ? '单路交流桩' : 
+                             key === '08' ? '十路智慧款V3' : 
+                             '— —'}}</el-col>
+                         <el-col :span="14"><el-input-number v-model="payTemData['00'][key]" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
                      </el-row>
                 </el-col>
                 <el-col :span="12" class="blueBooth">
                     <div class="title">蓝牙设备模板</div>
-                    <!-- <el-row class="item">
-                         <el-col :span="10">00出厂默认设置</el-col>
-                         <el-col :span="14"><el-input-number v-model="payTemData['01']['00']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
-                     </el-row>
                      <el-row class="item">
-                         <el-col :span="10">01十路智慧款</el-col>
-                        <el-col :span="14"><el-input-number v-model="payTemData['01']['01']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
-                     </el-row>
-                     <el-row class="item">
-                         <el-col :span="10">02电轿款</el-col>
-                        <el-col :span="14"><el-input-number v-model="payTemData['01']['02']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
-                     </el-row> -->
-                     <el-row class="item">
-                         <el-col :span="10">03脉冲板子</el-col>
+                        <el-col :span="10">03脉冲板子</el-col>
                         <el-col :span="14"><el-input-number v-model="payTemData['01']['03']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
                      </el-row>
-                     <!-- <el-row class="item">
-                         <el-col :span="10">04离线充值机</el-col>
-                        <el-col :span="14"><el-input-number v-model="payTemData['01']['04']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
-                     </el-row>
-                     <el-row class="item">
-                         <el-col :span="10">05十六路智慧款</el-col>
-                        <el-col :span="14"><el-input-number v-model="payTemData['01']['05']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
-                     </el-row>
-                     <el-row class="item">
-                         <el-col :span="10">06二十路智慧款</el-col>
-                        <el-col :span="14"><el-input-number v-model="payTemData['01']['06']" size="small" :min="0" :step="10" :max="1000" ></el-input-number></el-col>
-                     </el-row> -->
                 </el-col>
             </el-row>
             <div style="margin-top: 20px; text-align: center;"><el-button type="primary" icon="el-icon-document-checked" size="mini" @click="submitPayTem" >保存模板</el-button></div>
